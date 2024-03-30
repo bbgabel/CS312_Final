74c6e3e4896310b26ba7d9ffb27b4452f849a356
Nathan Bennick <natebenn@winter-park.cs.colostate.edu>
2024-03-30 17:02:25 -0600

    creating an outline for the homepage

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team Temp homepage</title>
+        <meta name="description" content="Final project for CS312 at CSU">
+        <meta name="keywords" content="HTML5, CS312, Colorado State University, Final Project, Group Project">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+        <div id="navbar">
+            <nav>
+            </nav>
+        </div>
+    </body>
+</html>
