e33d6bdc6f9336485f4db6c51601e31c734cc704
Nathan Bennick <natebenn@washington-dc.cs.colostate.edu>
2024-04-01 12:13:27 -0600

    home page work, creating about and color generator pages

--- homepage.php
+++ homepage.php
@@ -1,16 +1,46 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Team Temp homepage</title>
-        <meta name="description" content="Final project for CS312 at CSU">
-        <meta name="keywords" content="HTML5, CS312, Colorado State University, Final Project, Group Project">
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="style.css" type="text/css">
-    </head>
-    <body>
-        <div id="navbar">
-            <nav>
-            </nav>
+
+<head>
+    <title>Team Temp homepage</title>
+    <meta name="description" content="Final project for CS312 at CSU">
+    <meta name="keywords" content="HTML5, CS312, Colorado State University, Final Project, Group Project">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <div id="navbar">
+        <nav>
+            <a href="#">Homepage</a>
+            <a href="./about.php">About us</a>
+            <a href="./colorgenerator.php">Color Coordinate Generator</a>
+        </nav>
+    <div>
+    <div id="header">
+    <h1>Welcome to our home page!</h1>
+    </div>
+    <main>
+        <div id="introparagraph">
+            <p>Team temp was created for the purpose of the Final Project in CS312 
+                at Colorado State University. We seek to grow our understanding of 
+                web development and team skills by working together on this project
+                to make us better programmers for our future work. Please click on the
+                about us page to learn more about our team members, where we are from, and 
+                what we week to do with our skills in programming. 
+            </p>
+            <p id="Color generator explaination">
+                We have created a color coordinate generator. Given that your input of columns (n), rows (m), 
+                and colors (X) are all within the allowed values, this page will display two tables. The first
+                table will be 2 columns by X rows, where X is the number of colors that you provided. The 
+                second table will have a table nxm. These cells will be labeled, and you will be able to
+                see the selected colors from the first table. Finally, there will be a button near the 
+                bottom of the page that will allow you to see a printable version of your tables. 
+            </p>
         </div>
-    </body>
+    </main>
+    
+
+</body>
+
 </html>
