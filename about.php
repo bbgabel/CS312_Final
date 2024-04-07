fe060d58f1a0e9a5b2142145fd9669919fd14480
glacierunderground <72990793+glacierunderground@users.noreply.github.com>
2024-04-07 17:26:41 -0600

    Sean F Bio and printview changes
    Updated my bio and modified printview to fit a single 8.5x11

--- about.php
+++ about.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
+<?php
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <head>
     <title>About Us - Team Temp</title>
     <meta name="description" content="About the members of Team Temp">
@@ -19,6 +24,7 @@
     <div id="header">
         <h1>About Us</h1>
     </div>
+    <div id="background">
     <main>
         <div id="team-members">
             <div class="member">
@@ -26,8 +32,17 @@
                 <p>I'm Jeremiah, I'm a senior here at Colorado State University. I work at the United States Department of Agrictulture (USDA) as Programmer, and I aspire to be a full-stack software developer.</p>
                 <img src="./img/JG.jpeg" alt="Jeremiah">
             </div>
+<<<<<<< Updated upstream
+=======
+            <div class="member">
+                <h2>Sean Fyola</h2>
+                <p>I'm Sean, and I'm a student here at CSU. I have recently taken the jump into freelancing, where I primarily do PLC control and SCADA development. </p>
+                <img src="./img/SF.jpg" alt="Sean">
+            </div>
+>>>>>>> Stashed changes
         </div>
     </main>
+</div>
 </body>
 
 </html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<<<<<<< Updated upstream
+=======
+<?php
+header("Location: ./homepage.php");
+die();
+?>
+>>>>>>> Stashed changes
